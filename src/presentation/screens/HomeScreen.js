b10d5dcb9a507dd89c2ad11793fecbf9c41c53a0
Lawrence Macharia <macharialawrence2011@gmail.com>
2022-02-23 07:36:06 +0300

    removed the load more code

--- src/presentation/screens/HomeScreen.js
+++ src/presentation/screens/HomeScreen.js
@@ -1,19 +1,19 @@
 import React, { useState, useEffect } from 'react'
 import { TouchableWithoutFeedback, StyleSheet } from 'react-native'
 import { useSelector, useDispatch } from 'react-redux';
-import { Layout, Input, Icon, List, ListItem, Avatar, Button } from '@ui-kitten/components';
+import { Layout, Input, Icon, List, ListItem, Avatar, Button, Text } from '@ui-kitten/components';
 
-import { setInitialPosts, onLoadMorePosts } from 'slices/postSlice'
-import { selectPosts } from 'selectors/post'
+import { setInitialPosts, setLoadMore } from 'slices/postSlice'
+import { fetchPosts } from 'api/post.service';
+import { selectLoadMore, selectPosts } from 'selectors/post'
 
 const HomeScreen = ({ navigation }) => {
 
   const dispatch = useDispatch();
+  const loading = useSelector(selectLoadMore)
   const posts = useSelector(selectPosts)
 
   const [value, setValue] = useState('');
-  const [loadingMore, setLoadingMore] = useState(false);
-  const [allLoaded, setAllLoaded] = useState(false);
 
 
   const renderIcon = (props) => (
@@ -45,57 +45,12 @@ const HomeScreen = ({ navigation }) => {
     />
   );
 
-  const loadMoreResults = async () => {
-    console.log('loadMoreResults')
-
-    if (loadingMore || allLoaded)
-      return
-
-    if (data.length == 100)
-      setAllLoaded(true)
-
-    setLoadingMore(true)
-
-    const newItems = fetchMore(10)
-
-    await delay(1000)
-
-    await processNewResults(newItems)
-
-    setLoadingMore(false);
-  }
-
-  const fetchMore = (total) => {
-    // data.push()
-    // for (let item of newItems) {
-    //   json.push(item);
-    // }
-
-    // for (let index = 0; index < total; index++) {
-    //   console.log(myArray[index]);
-    // }
-
-    return new Array(total).fill({
-      title: 'Title for Item',
-      description: 'Description for Item',
-    });
-  }
-
-  const processNewResults = async (newItems) => {
-    newItems.forEach(function (value) {
-      console.log(value);
-    });
-  }
+  useEffect(() => {
+    console.log('loading', loading)
+  }, [loading])
 
   useEffect(() => {
-    const initialData = new Array(10).fill({
-      title: 'Title for Item',
-      description: 'Description for Item',
-    });
-
-    // setData(initialData)
-    dispatch(setInitialPosts(initialData))
-
+    dispatch(setInitialPosts())
   }, [])
 
   return (
@@ -109,9 +64,24 @@ const HomeScreen = ({ navigation }) => {
       />
       <List
         style={{ flex: 1 }}
+        keyExtractor={(item, index) => "post_" + index}
         data={posts}
         renderItem={renderItem}
-        onEndReached={() => console.log('loadMoreResults')}
+        scrollEventThrottle={250}
+        // onEndReached={() => setLoadMore(true)}
+        onEndReached={info => {
+          dispatch(setLoadMore())
+
+          setInterval(() => dispatch(fetchPosts(posts.length)), 1000)
+        }}
+        onEndReachedThreshold={0.01}
+        ListFooterComponent={
+          <Layout style={styles.footer}>
+            {loading &&
+              <Text style={styles.footerText}>Loading More...</Text>
+            }
+          </Layout>
+        }
       />
     </Layout>
   )
@@ -127,6 +97,12 @@ const styles = StyleSheet.create({
   avatar: {
     margin: 8,
   },
+  footer: {
+    padding: 15,
+  },
+  footerText: {
+    fontWeight: '600',
+  }
 });
 
 export default HomeScreen
