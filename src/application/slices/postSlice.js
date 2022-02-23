05a0ee619cb7e77452258e9e06b358f48676ce80
Lawrence Macharia <macharialawrence2011@gmail.com>
2022-02-23 07:36:47 +0300

    code improvement

--- src/application/slices/postSlice.js
+++ src/application/slices/postSlice.js
@@ -1,6 +1,10 @@
 import { createSlice } from '@reduxjs/toolkit'
 
+import { fetchPosts } from 'api/post.service'
+
 const initialState = {
+  loadMore: false,
+  allLoaded: false,
   posts: []
 }
 
@@ -8,12 +12,63 @@ const postSlice = createSlice({
   name: 'post',
   initialState,
   reducers: {
-    setInitialPosts: (state, action) => {
-      state.posts = action.payload
+    setLoadMore: (state, action) => {
+      console.log('setLoadMore', action.payload)
+
+      state.loadMore = action.payload
     },
-    onLoadMorePosts: (state, action) => { }
+    setInitialPosts: (state, action) => {
+
+      const initialData = new Array(10).fill({
+        title: 'Title for Item',
+        description: 'Description for Item',
+      });
+
+      state.posts = initialData
+    },
+    // onLoadMorePosts: (state, action) => {
+    //   console.log('onLoadMorePosts:loadMore', state.loadMore)
+
+    //   if (state.loadMore || state.allLoaded)
+    //     return
+
+    //   state.loadMore = true
+
+    //   // let maxPosts = state.posts.length + 10
+    //   // for (let index = state.posts.length; index < maxPosts; index++) {
+    //   //   state.posts.push({
+    //   //     title: 'New Title for Item ',
+    //   //     description: 'Description for Item',
+    //   //   })
+    //   // }
+    //   console.log('posts count', state.posts.length)
+    //   console.log(fetchMorePosts())
+    //   state.loadMore = false
+    //   state.allLoaded = state.posts.length === 100 ? true : false
+    // }
+  },
+  extraReducers: (builder) => {
+    builder.addCase(fetchPosts.pending, (state, action) => {
+      // state.loadMore = true
+    });
+    builder.addCase(fetchPosts.fulfilled, (state, action) => {
+
+      action.payload.forEach(function (value) {
+        state.posts.push(value)
+      });
+
+      state.loadMore = false
+      state.allLoaded = state.posts.length === 100 ? true : false
+
+    });
+    builder.addCase(fetchPosts.rejected, (state, action) => {
+      state.loadMore = false
+      state.posts = []
+    });
   }
 })
 
-export const { setInitialPosts, onLoadMorePosts } = postSlice.actions
+
+
+export const { setInitialPosts, setLoadMore } = postSlice.actions
 export default postSlice.reducer
